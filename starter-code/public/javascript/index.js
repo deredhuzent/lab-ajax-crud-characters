326b152ee79c3f097b161e6efc0ca6d921399b80
deredhuzent <hakurascot@gmail.com>
2019-04-24 19:57:08 -0500

    FetchAll, FetchOne, Delete Done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,67 @@
+//Botones para acceder a la lista de API
+const buttonFetchAll = document.getElementById('fetch-all')
+const buttonFetchOne = document.getElementById('fetch-one')
+
+//Botones para editar lista de API
+const buttonDelete = document.getElementById('delete-one')
+const buttonUpdate = document.getElementById('edit-character-form')
+const buttonCreate = document.getElementById('new-character-form')
+
 const charactersAPI = new APIHandler("http://localhost:8000")
+//BASE_URL --->>> APIhandler URL que da en la consola
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
+window.onload = () => {
+
+  //FetchAll
+  buttonFetchAll.onclick = () => {
+    axios.get(charactersAPI.BASE_URL + '/characters')
+    .then(({data}) => {
+        console.log(data)
+    })
+    .catch(err => console.log((err)))
+}
+  
+//FetchOne
+  buttonFetchOne.onclick = () => {
+    id = document.getElementById('character-id').value
+    axios.get(`${charactersAPI.BASE_URL}` + `/characters/${id}`)
+    .then((data) => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
   }
   
-  document.getElementById('fetch-one').onclick = function(){
-    
+  //Delete
+  buttonDelete.onclick = () => {
+    id = document.getElementById('character-id-delete').value
+    axios.delete(`${charactersAPI.BASE_URL}` + `/characters/${id}`)
+    .then((data) => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
   }
   
-  document.getElementById('delete-one').onclick = function(){
-        
+  //Create
+  buttonCreate.onclick = () => {
+    id = document.getElementById('new-character-form').value
+
+    //inputs
+    name = document.querySelector('input[name = "name"]').value
+    occupation = document.querySelector('input[occupation = "occupation"]').value
+    weapon = document.querySelector('input[weapon = "weapon"]').value
+    //cartoon = document.querySelector('input[cartoon] = "checkbox"').value
+
+    axios.post(`${charactersAPI.Base_URL}${id}`, {name, occupation, weapon})
+    .then((data) => {
+      console.log(data)
+    })
+    .catch(err => console.log(err))
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  //Edit
+  buttonUpdate.onsubmit = () => {
+    id = document.getElementById('')       
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+}
